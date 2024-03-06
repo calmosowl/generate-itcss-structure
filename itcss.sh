6c5087143442c85ba3f34e58ef5dba4dacf9ceec
Artem Kyselov <calmos.owl@gmail.com>
2024-03-06 20:46:47 +0200

    add verbose output

--- itcss.sh
+++ itcss.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-mkdir 0-settings && touch 0-settings/_index.scss
-mkdir 1-tools && touch 1-tools/_index.scss
-mkdir 2-generic && touch 2-generic/_index.scss
-mkdir 3-elements && touch 3-elements/_index.scss
-mkdir 4-objects && touch 4-objects/_index.scss
-mkdir 5-components && touch 5-components/_index.scss
-mkdir 6-utils && touch 6-utils/_index.scss
-touch main.scss
-echo '@import "0-settings/index";' > main.scss
-echo '@import "1-tools/index";' >> main.scss
-echo '@import "2-generic/index";' >> main.scss
-echo '@import "3-elements/index";' >> main.scss
-echo '@import "4-objects/index";' >> main.scss
-echo '@import "5-components/index";' >> main.scss
-echo '@import "6-utils/index";' >> main.scss
+mkdir -v 0-settings && touch 0-settings/_index.scss
+mkdir -v 1-tools && touch 1-tools/_index.scss
+mkdir -v 2-generic && touch 2-generic/_index.scss
+mkdir -v 3-elements && touch 3-elements/_index.scss
+mkdir -v 4-objects && touch 4-objects/_index.scss
+mkdir -v 5-components && touch 5-components/_index.scss
+mkdir -v 6-utils && touch 6-utils/_index.scss
+
+touch main.scss;
+echo '@import "0-settings/index";' > main.scss;
+echo '@import "1-tools/index";' >> main.scss;
+echo '@import "2-generic/index";' >> main.scss;
+echo '@import "3-elements/index";' >> main.scss;
+echo '@import "4-objects/index";' >> main.scss;
+echo '@import "5-components/index";' >> main.scss;
+echo '@import "6-utils/index";' >> main.scss;
